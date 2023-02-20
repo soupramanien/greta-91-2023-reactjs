3b889dc7b27d06bd6328215cdb3c734e82812f1c
soupramanien <soupramani@yahoo.fr>
2023-02-20 10:52:21 +0100

    validation de données ajoutée

--- src/product/ProductForm.js
+++ src/product/ProductForm.js
@@ -1,22 +1,66 @@
 import { useState } from "react";
 
 export default function ProductForm(props) {
+    //contraintes
+    //name :
+    //doit contenir au moins 2 caracteres
+    //ne doit pas contenir des caracteres suivants : %$@£
     const [name, setName] = useState("");
+    //contraintes
+    //price :
+    //doit être un nombre
+    //doit être superieur à 0.0
     const [price, setPrice] = useState("");
-    const handleChange = (event)=>{
+    const [nameMessage, setNameMessage] = useState("")
+    const [priceMessage, setPriceMessage] = useState("")
+
+    const handleChange = (event) => {
+        let value = event.target.value;
         switch (event.target.name) {//"name"|"price"
             case "name":
-                setName(event.target.value)
+                if (/^[^%$@£]*$/.test(value)) {
+                    setName(value)
+                    if (value.length < 2) {
+                        setNameMessage("il manque " + (2 - value.length) + " caractere(s)");
+                    }
+                    else {
+                        setNameMessage("");
+                    }
+                }
                 break;
             case "price":
-                setPrice(event.target.value)
+                // console.log(parseFloat(value));
+                if (value === "0") {
+                    setPrice(0)
+                } else if (value === "") {
+                    setPrice("")
+                }
+                value = parseFloat(value);//
+                if (!isNaN(value) && value > 0) {
+                    setPrice(value)
+                    setPriceMessage("")
+                }
+                else {
+                    setPriceMessage("prix n'est pas valide")
+                }
                 break;
             default:
                 break;
         }
     }
-    const handleSubmit = (event)=>{
+    const handleSubmit = (event) => {
         event.preventDefault();
+        let error = false;
+        if (!/^[^%$@£]{2,}$/.test(name)) {
+            error = true
+        }
+        if (price <= 0) {
+            error = true
+        }
+        if (error) {
+            alert("les données ne sont pas valide !")
+            return;
+        }
         const product = {
             name: name,
             price: price
@@ -27,22 +71,28 @@ export default function ProductForm(props) {
         <form>
             <div>
                 <label htmlFor="name">Nom</label>
-                <input 
-                    type="text" 
-                    id="name" 
-                    value={name} 
-                    name="name" 
-                    onChange={handleChange}/>
+                <input
+                    type="text"
+                    id="name"
+                    value={name}
+                    name="name"
+                    onChange={handleChange} />
+                {nameMessage.length > 0 &&
+                    <p className="text-danger">{nameMessage}</p>
+                }
             </div>
             <div>
                 <label htmlFor="price">Prix</label>
-                <input 
-                    type="number" 
-                    id="price" 
-                    value={price} 
+                <input
+                    type="number"
+                    id="price"
+                    value={price}
                     name="price"
                     step="0.01"
-                    onChange={handleChange}/>
+                    onChange={handleChange} />
+                {priceMessage.length > 0 &&
+                    <p className="text-danger">{priceMessage}</p>
+                }
             </div>
             <div>
                 <button onClick={handleSubmit}>Envoyer</button>
